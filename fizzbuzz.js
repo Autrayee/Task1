7f43ea7310bd7a593327bcd3c845c294d563ef4b
Autrayee Batsas <autrayee.batsas@capgemini.com>
2021-10-01 15:40:57 +0530

    fizzbuzz created

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+function fizzBuzz2(n) {
+    for (let i = 1; i <= n; i++) {
+      let str = "";
+  
+      if (i % 3 === 0) str += "fizz"
+      if (i % 5 === 0) str += "buzz"
+      if (str === "") str = i;
+    
+      console.log(str);
+    }
+  }
