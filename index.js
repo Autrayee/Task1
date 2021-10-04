dcc70acd73f8e1af8dc5a23a2d577177a43ec54b
Autrayee Batsas <autrayee.batsas@capgemini.com>
2021-10-04 09:21:29 +0530

    added 3 images

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function readFilePromise(fileLocation) {
   
   
   
-  async function getRoboPic() {
+  /*async function getRoboPic() {
     try {
       const randomText = await readFilePromise('./name.txt')
       console.log(`Robo is ${randomText}`)
@@ -107,5 +107,63 @@ function readFilePromise(fileLocation) {
   
   // getRoboPic().then(() => {
   //   console.log('3. end')
-  // })
+  // })*/
+
+ 
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+function randomString(length) {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+//Promise.all
+function writeFilePromise(fileLocation, result) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileLocation, result, (err) => {
+      if (err) {
+        reject("not able to write to the file");
+      }
+      resolve();
+    });
+  });
+}
+async function getRandomPic() {
+  try {
+    const res1 = await superagent.get(
+      `https://robohash.org/${randomString(6)}`
+    );
+    console.log("Random image is ", res1.request.url);
+    const res2 = await superagent.get(
+      `https://robohash.org/${randomString(6)}`
+    );
+    console.log("Random image is ", res2.request.url);
+    const res3 = await superagent.get(
+      `https://robohash.org/${randomString(6)}`
+    );
+    console.log("Random image is ", res3.request.url);
+
+    const all = await Promise.all([res1, res2, res3]);
+    const images = all.map((el) => el.request.url);
+    console.log(images);
+    await writeFilePromise("./RoboImage.txt", images.join("\n"));
+    console.log("sucessfully written the file");
+  } catch (err) {
+    throw err;
+  }
+}
+
+(async () => {
+  try {
+    await getRandomPic();
+  } catch (err) {
+    console.log(err);
+  }
+})();
   
