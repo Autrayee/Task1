5be4a26d58c5918abc1ad837706a27cf2f6564ca
Autrayee Batsas <autrayee.batsas@capgemini.com>
2021-10-04 11:41:54 +0530

    added binary search test

--- binary.js
+++ binary.js
@@ -1,38 +1,24 @@
 
 
-let iterativeFunction = function (arr, x) {
-  
-    let start=0, end=arr.length-1;
-         
-    // Iterate while start not meets end
-    while (start<=end){
- 
-        // Find the mid index
-        let mid=Math.floor((start + end)/2);
-  
-        // If element is present at mid, return True
-        if (arr[mid]===x) return true;
- 
-        // Else look in left or right half accordingly
-        else if (arr[mid] < x)
-             start = mid + 1;
-        else
-             end = mid - 1;
+const binSearch = (arr, key) => {
+    if (!Array.isArray(arr) || typeof key !== "number")
+        throw new Error("Invalid input");
+    const sortedArray = [...arr].sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] !== sortedArray[i]) throw new Error("Array must be sorted");
+
+    let lower = 0;
+    let upper = arr.length;
+
+    while (lower <= upper) {
+        const middle = lower + Math.floor((upper - lower) / 2);
+        if (key === arr[middle]) return middle + 1;
+        if (key > arr[middle]) lower = middle + 1;
+        else upper = middle - 1;
     }
-  
-    return false;
-}
-  
-// Driver code
-let arr = [1, 3, 5, 7, 8, 9];
-let x = 5;
-  
-if (iterativeFunction(arr, x, 0, arr.length-1))
-    document.write("Element found!<br>");
-else document.write("Element not found!<br>");
-  
-//x = 6;
-  
-//if (iterativeFunction(arr, x, 0, arr.length-1))
-  //  document.write("Element found!<br>");
-//else document.write("Element not found!<br>");
+    return -1;
+};
+
+//console.log(binSearch([1,3,4,5,6],6));
+module.exports = binSearch;
